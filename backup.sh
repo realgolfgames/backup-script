3958f07887bc499b4294f5161390a0e40ab2b218
Julian Hammer <jh@moinjulian.com>
2024-04-22 22:03:54 +0100

    Update Backup Script to also export

--- backup.sh
+++ backup.sh
@@ -10,6 +10,7 @@ else
     echo "Error: .env file not found."
     exit 1
 fi
+
 # Get current timestamp in the desired format
 timestamp=$(date "+%H-%M-%S")
 
@@ -19,15 +20,16 @@ month=$(date "+%m")
 day=$(date "+%d")
 
 # Ensure the folders exist, create them if not
-mkdir -p "backups/${year}/${month}/${day}"
+mkdir -p "../backup/ac-onr5u54-shard-00"
+mkdir -p "../backup/ac-tl1blv7-shard-00"
 
 # Run mongoexport command with timestamp in the output file name and store in the organized folder
-mongoexport --uri "${MONGODB_URI}" --collection users --type JSON --out "backups/${year}/${month}/${day}/${timestamp}.json"
+mongoexport --uri="${MONGODB_URI_USERS}" --collection="users" --type="JSON" --out="../backup/ac-onr5u54-shard-00/${year}-${month}-${day}_${timestamp}.import.json"
+mongoexport --uri="${MONGODB_URI_USERS}" --collection="users" --type="JSON" --out="../backup/ac-onr5u54-shard-00/${year}-${month}-${day}_${timestamp}.readable.json" --pretty
+mongoexport --uri="${MONGODB_URI_COURSES}" --collection="courses" --type="JSON" --out="../backup/ac-tl1blv7-shard-00/${year}-${month}-${day}_${timestamp}.import.json"
+mongoexport --uri="${MONGODB_URI_COURSES}" --collection="courses" --type="JSON" --out="../backup/ac-tl1blv7-shard-00/${year}-${month}-${day}_${timestamp}.readable.json" --pretty
 
 # Wait for the export to complete
 while pgrep -f "mongoexport" >/dev/null; do
     sleep 1
 done
-
-# # Use killall to terminate the Terminal application
-# killall Terminal
