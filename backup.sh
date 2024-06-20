1d8eed3e0b76c1a7de4d874860b6884010ce7ceb
Julian Hammer <jh@moinjulian.com>
2024-06-20 15:06:51 +0100

    Update backup script to include export for quizles collection

--- backup.sh
+++ backup.sh
@@ -22,18 +22,23 @@ day=$(date "+%d")
 # Ensure the folders exist, create them if not
 mkdir -p "../backup/users"
 mkdir -p "../backup/courses"
+mkdir -p "../backup/quizles"
 
 # Get absolute paths
 users_backup_dir="$(cd ../backup/users && pwd)"
 courses_backup_dir="$(cd ../backup/courses && pwd)"
+quizles_backup_dir="$(cd ../backup/quizles && pwd)"
 
 # Debug output for directories
 echo "Users backup directory: ${users_backup_dir}"
 echo "Courses backup directory: ${courses_backup_dir}"
+echo "Quizles backup directory: ${quizles_backup_dir}"
 
 # Run mongoexport command with timestamp in the output file name and store in the organized folder
 mongoexport --uri="mongodb+srv://moinjulian:${MONGODB_PASSWORD}@cluster0.w2umjjr.mongodb.net/prod?retryWrites=true&w=majority&appName=Cluster0" --collection="users" --type="JSON" --out="${users_backup_dir}/${year}/${month}/${day}/${timestamp}.import.json"
 mongoexport --uri="mongodb+srv://moinjulian:${MONGODB_PASSWORD}@cluster0.w2umjjr.mongodb.net/prod?retryWrites=true&w=majority&appName=Cluster0" --collection="users" --type="JSON" --out="${users_backup_dir}/${year}/${month}/${day}/${timestamp}.readable.json" --pretty
+mongoexport --uri="mongodb+srv://moinjulian:${MONGODB_PASSWORD}@cluster0.w2umjjr.mongodb.net/prod?retryWrites=true&w=majority&appName=Cluster0" --collection="quizles" --type="JSON" --out="${quizles_backup_dir}/${year}/${month}/${day}/${timestamp}.import.json"
+mongoexport --uri="mongodb+srv://moinjulian:${MONGODB_PASSWORD}@cluster0.w2umjjr.mongodb.net/prod?retryWrites=true&w=majority&appName=Cluster0" --collection="quizles" --type="JSON" --out="${quizles_backup_dir}/${year}/${month}/${day}/${timestamp}.readable.json" --pretty
 mongoexport --uri="mongodb+srv://moinjulian:${MONGODB_PASSWORD}@cluster0.w2umjjr.mongodb.net/prod?retryWrites=true&w=majority&appName=Cluster0" --collection="courses" --type="JSON" --out="${courses_backup_dir}/${year}/${month}/${day}/${timestamp}.import.json"
 mongoexport --uri="mongodb+srv://moinjulian:${MONGODB_PASSWORD}@cluster0.w2umjjr.mongodb.net/prod?retryWrites=true&w=majority&appName=Cluster0" --collection="courses" --type="JSON" --out="${courses_backup_dir}/${year}/${month}/${day}/${timestamp}.readable.json" --pretty
 
